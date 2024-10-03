fa5288d91f724b9ce3627bee268f5e468721d9bc
TakumaYyyyyy <rokkuman9931@gmail.com>
2024-10-03 14:00:19 +0900

    ds

--- Assets/Scenes/PlayScenecamera/LineTrigger.cs
+++ Assets/Scenes/PlayScenecamera/LineTrigger.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class LineTrigger : MonoBehaviour
 {
+    // プレイヤーコントローラーの参照
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        // プレイヤーコントローラーを取得
+        playerController = FindObjectOfType<PlayerController>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -10,7 +18,13 @@ public class LineTrigger : MonoBehaviour
         {
             // プレイヤーが触れた時の処理
             Debug.Log("プレイヤーがラインに触れました。");
-            
+
+            // プレイヤーの速度を取得して増加させる
+            if (playerController != null)
+            {
+                playerController.playerspeed += 30f; // 速度を増加（5fは任意の増加値）
+                Debug.Log("新しいプレイヤー速度: " + playerController.playerspeed);
+            }
 
             // 他の処理をここに追加
         }
